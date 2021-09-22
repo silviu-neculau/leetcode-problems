cb9f319d50276d169943d842e47fc730103adfe1
Neculau Silviu <silviu.mihai.neculau@gmail.com>
2021-09-22 12:26:14 +0300

    Add KidsWith...

---
+++ src/com/leetcode/problems/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,22 @@
+package com.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> theMostCandies = new ArrayList<>();
+        int temp = 0, max = 0;
+        for(int i = 0; i < candies.length; i++)
+            if(max < candies[i])
+                max = candies[i];
+        for (int j = 0; j < candies.length; j++) {
+            temp = candies[j] + extraCandies;
+            if (temp >= max) theMostCandies.add(true);
+            else theMostCandies.add(false);
+
+            temp = 0;
+        }
+        return theMostCandies;
+    }
+}
