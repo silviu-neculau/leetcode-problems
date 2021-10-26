fd5793fa5a81468b8a2afbcadb8537c88d083a1d
Neculau Silviu <silviu.mihai.neculau@gmail.com>
2021-10-26 20:35:28 +0300

    Add ThirdMaximumNumber

---
+++ src/com/leetcode/problems/SetMatrixZeroes.java
@@ -0,0 +1,2 @@
+package com.leetcode.problems;public class SetMatrixZeroes {
+}
---
+++ src/com/leetcode/problems/ThirdMaximumNumber.java
@@ -0,0 +1,31 @@
+package com.leetcode.problems;
+
+import java.util.Arrays;
+
+public class ThirdMaximumNumber {
+    public static void main(String[] args) {
+        System.out.println((getThirdMax(new int[] {1,2,-2147483648})));
+    }
+
+    public static int getThirdMax(int[] nums) {
+        int firstMax = Integer.MIN_VALUE;
+        int thirdMax = Integer.MIN_VALUE;
+        int secondtMax = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] > firstMax) {
+                thirdMax = secondtMax;
+                secondtMax = firstMax;
+                firstMax = nums[i];
+            } else if (nums[i] > secondtMax && nums[i] != firstMax) {
+                thirdMax = secondtMax;
+                secondtMax = nums[i];
+            } else if (nums[i] > thirdMax && nums[i] != secondtMax && nums[i] != firstMax)
+                thirdMax = nums[i];
+        }
+        if (nums.length < 3) return firstMax;
+
+        return thirdMax;
+
+    }
+}
