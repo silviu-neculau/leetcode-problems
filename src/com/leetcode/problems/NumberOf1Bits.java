c668fafc3f67410f90c9e8359004bb271ef2285e
Neculau Silviu <silviu.mihai.neculau@gmail.com>
2022-09-05 16:02:59 +0300

    Fix first round of review

--- src/com/leetcode/problems/NumberOf1Bits.java
+++ src/com/leetcode/problems/NumberOf1Bits.java
@@ -9,11 +9,9 @@ public class NumberOf1Bits {
 
   public static int hammingWeight(int n) {
     int count = 0;
-    String str = Integer.toBinaryString(n);
-    for (int i = 0; i < str.length(); i++) {
-      if (str.charAt(i) == '1') {
-        count++;
-      }
+    while (n!=0){
+      count= count + (n & 1);
+      n = n >> 1;
     }
     return count;
   }
