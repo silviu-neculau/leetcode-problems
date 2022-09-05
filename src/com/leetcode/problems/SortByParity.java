99329b0ff3d1a5850fdc4778f07a3fd9f94f0416
Neculau Silviu <silviu.mihai.neculau@gmail.com>
2022-09-05 15:07:58 +0300

    Fix first round of review

--- src/com/leetcode/problems/SortByParity.java
+++ src/com/leetcode/problems/SortByParity.java
@@ -2,20 +2,25 @@ package com.leetcode.problems;
 
 // https://leetcode.com/problems/sort-array-by-parity/
 
+import java.util.Arrays;
+
 public class SortByParity {
-  public int[] sortArrayByParity(int[] nums) {
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(sortArrayByParity(new int[] {3, 1, 2, 4})));
+  }
+
+  public static int[] sortArrayByParity(int[] nums) {
     int even = 0;
-    int odd = 0;
-    int[] ans = new int[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      if (nums[i] % 2 == 0) {
-        ans[even] = nums[i];
-        even++;
-      } else {
-        ans[nums.length - 1 - odd] = nums[i];
-        odd++;
+    int odd = nums.length - 1;
+    while (even < odd) {
+      if (nums[even] % 2 > nums[odd] % 2) {
+        int temp = nums[even];
+        nums[even] = nums[odd];
+        nums[odd] = temp;
       }
+      if (nums[even] % 2 == 0) even++;
+      if (nums[odd] % 2 == 1) odd--;
     }
-    return ans;
+    return nums;
   }
 }
