f89349f6987988b4245afa27cf2b1618bc45a691
silviu-neculau <silviu.mihai.neculau@gmail.com>
2022-09-11 15:56:05 +0300

    Second round of review (#16)

---
+++ src/main/java/com/leetcode/problems/HighestAltitude.java
@@ -0,0 +1,17 @@
+package com.leetcode.problems;
+
+
+public class HighestAltitude {
+    public static void main(String[] args) {
+        System.out.println(largestAltitude(new int[] {-4, -3, -2, -1, 4, 3, 2}));
+    }
+
+    public static int largestAltitude(int[] gain) {
+        int currentPoint = 0, highestAltitude = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currentPoint += gain[i];
+            if (highestAltitude < currentPoint) highestAltitude = currentPoint;
+        }
+        return highestAltitude;
+    }
+}
