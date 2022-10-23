2985f563985ef1d4f189e0c1c92511c37c0cc967
Neculau Silviu <silviu.mihai.neculau@gmail.com>
2022-10-23 19:04:37 +0300

    Added Paranthesis

--- src/main/java/com/leetcode/problems/HighestAltitude.java
+++ src/main/java/com/leetcode/problems/HighestAltitude.java
@@ -1,6 +1,5 @@
 package com.leetcode.problems;
 
-
 public class HighestAltitude {
     public static void main(String[] args) {
         System.out.println(largestAltitude(new int[] {-4, -3, -2, -1, 4, 3, 2}));
--- src/main/java/com/leetcode/problems/SortByParity2.java
+++ src/main/java/com/leetcode/problems/SortByParity2.java
@@ -5,12 +5,12 @@ package com.leetcode.problems;
 public class SortByParity2 {
 
     public static void main(String[] args) {
-        System.out.println((sortArrayByParityII(new int[] {1, 2, 5})));
+        System.out.println((sortArrayByParityII(new int[] {1, 2, 5, 7})));
     }
 
     public static int[] sortArrayByParityII(int[] nums) {
         int j = 1;
-        for (int i = 0; i < nums.length; i += 2)
+        for (int i = 0; i < nums.length; i += 2) {
             if (nums[i] % 2 == 1) {
                 while (nums[j] % 2 == 1) {
                     j += 2;
@@ -19,7 +19,7 @@ public class SortByParity2 {
                 nums[i] = nums[j];
                 nums[j] = tmp;
             }
-
+        }
         return nums;
     }
 }
