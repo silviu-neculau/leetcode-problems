01ea2e369a9c02a62bbf2acc9f90bd1e542cba76
Neculau Silviu <silviu.mihai.neculau@gmail.com>
2022-11-11 20:51:01 +0200

    Renamed variable

--- src/main/java/com/leetcode/problems/Mismatch.java
+++ src/main/java/com/leetcode/problems/Mismatch.java
@@ -11,7 +11,7 @@ public class Mismatch {
     }
 
     public static int[] findErrorNums(int[] nums) {
-        int dup = -1, missing = -1;
+        int duplicate = -1, missing = -1;
         for (int i = 1; i <= nums.length; i++) {
             int count = 0;
             for (int j = 0; j < nums.length; j++) {
@@ -20,11 +20,11 @@ public class Mismatch {
                 }
             }
             if (count == 2) {
-                dup = i;
+                duplicate = i;
             } else if (count == 0) {
                 missing = i;
             }
         }
-        return new int[] {dup, missing};
+        return new int[] {duplicate, missing};
     }
 }
