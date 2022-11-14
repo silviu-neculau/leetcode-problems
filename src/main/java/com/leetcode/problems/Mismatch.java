b2b160bfa7c59a68e63f3c1c389194a3224b882d
silviu-neculau <silviu.mihai.neculau@gmail.com>
2022-11-14 11:40:59 +0200

    Add Mismatch (#46)

---
+++ src/main/java/com/leetcode/problems/Mismatch.java
@@ -0,0 +1,30 @@
+package com.leetcode.problems;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/set-mismatch/
+
+public class Mismatch {
+
+    public static void main(String[] args) {
+        System.out.println((Arrays.toString(findErrorNums(new int[] {1, 2, 2}))));
+    }
+
+    public static int[] findErrorNums(int[] nums) {
+        int duplicate = -1, missing = -1;
+        for (int i = 1; i <= nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] == i) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                duplicate = i;
+            } else if (count == 0) {
+                missing = i;
+            }
+        }
+        return new int[] {duplicate, missing};
+    }
+}
